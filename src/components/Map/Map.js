024ce110f6cbbd4ef8cfcb6604d803479c9817f0
Daniel Gonzalez <daa.gonzalez@gmail.com>
2024-09-22 14:38:15 -0500

    feat: add container for the map

---
+++ src/components/Map/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+    return (
+        <div data-testid="map-container">
+        </div>
+    );
+};
+
+export default Map;
